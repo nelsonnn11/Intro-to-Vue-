7537384f981f7328ac2c0608aaf29634bde6a896
124norellana@frhsd.com <124norellana@frhsd.com>
2024-02-23 09:49:47 -0500

    Lesson 5 completed

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
             inventory: 100,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyster'],
+            variants: {
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            }
         }
     }
 })
