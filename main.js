31174c7f18262d2460ec27ef71c7801407ba0d79
124norellana@frhsd.com <124norellana@frhsd.com>
2024-02-26 10:18:41 -0500

    Lesson 5 Completed

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ const app = Vue.createApp({
             inventory: 100,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyster'],
-            variants: {
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'}
-            }
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ],
+            sizes: ['xsmall', 'small', 'medium', 'large', 'xlarge']
         }
     }
 })
